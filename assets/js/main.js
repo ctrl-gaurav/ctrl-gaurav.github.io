5f7605702b2bb94a5e1a30c32d600f6ea68a2f12
ctrl-gaurav <gauravhhh30@gmail.com>
2023-11-08 17:36:10 +0530

    New Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,110 +1,391 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+// About Me content is the one by default shown
+$('#educationContent').hide();
+$('#publicationsContent').hide();
+$('#experienceContent').hide();
+$('#conferencesContent').hide();
+$('#projectsContent').hide();
+$('#tutorialsContent').hide();
+$('#academicContent').hide();
+$('#particularContent').hide();
+/* Template
+$('#nameContent').hide();
+*/
+$('#theme').hide();
+$('#lan').hide();
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
+// Hides all the divs in the particular, unless the first one
+$('.particular-clickable').next().hide();
+$('.particular-clickable').next()[0].style.display="block";
+
+$(document).ready(function(){
+
+	$.getJSON("https://api.countapi.xyz/hit/ctrl-gaurav.github.io/api_key", function(response) {
+		$("#contadorVisitas").text(response.value);
+	});
+
+	// First time, check the theme
+	if(localStorage.getItem("theme") === null){
+		localStorage.theme = "light";
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+			localStorage.theme = "dark";
+	}
+	
+	// First time, check the locale
+	let userLang = navigator.language || navigator.userLanguage;
+	if(localStorage.getItem("lan") === null){
+		localStorage.lan = "en";
+		if (userLang.split('-')[0] == "es")
+			localStorage.lan = "es";
+	}
+
+	// Maybe first time or not, so load the localStorage value
+	$('<link>').appendTo('head').attr({
+		type: 'text/css', 
+		rel: 'stylesheet',
+		href: 'assets/css/light.css'
+	});
+	if (localStorage.theme == "dark") {
+		// Handle menu
+		$("link[href='assets/css/light.css']").remove();
+		$('<link>').appendTo('head').attr({
+			type: 'text/css', 
+			rel: 'stylesheet',
+			href: 'assets/css/dark.css'
+		});
+		$('#theme').empty().append("<i class='fa-duotone fa-lightbulb-slash'></i>");
+	}
+	// Done because light is the one by default
+	if(localStorage.lan == "es") {
+		$('#lan img').attr("src","/assets/img/es_flag.webp");
+		$('#lan').addClass("es");
+	}
+	updateLanguage();
+
+	// Handle 'About Me' content
+	$('#aboutme').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#aboutmeContent');
+		}
+
+	});
+
+	// Handle 'Education' content
+	$('#education').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#educationContent');
+		}
+	});
+
+	// Handle 'Publications' content
+	$('#publications').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#publicationsContent');
+		}
+	});
+
+	// Handle 'Blog' content
+	$('#tutorials').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#tutorialsContent');
+		}
+	});
+
+	// Handle 'Academic' content
+	$('#academic').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#academicContent');
+		}
+	});
+
+	// Handle 'Particular' content
+	$('#particular').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#particularContent');
+		}
+	});
+
+	// Handle 'Conferences' content
+	// $('#conferences').click(function(e) {
+
+	// 	// If the div has already the class active, no need to reload the divs...
+	// 	if(!$(e.target).hasClass('active')) {
+	// 		// Update navbar
+	// 		clearActiveLinks();
+	// 		activateLink(e);
+
+	// 		// Hide other contents
+	// 		clearActiveDivs();
+
+	// 		// Show current content
+	// 		activateDiv('#conferencesContent');
+	// 	}
+	// });
+
+	// Handle 'Experience' content
+	// $('#experience').click(function(e) {
+
+	// 	// If the div has already the class active, no need to reload the divs...
+	// 	if(!$(e.target).hasClass('active')) {
+	// 		// Update navbar
+	// 		clearActiveLinks();
+	// 		activateLink(e);
+
+	// 		// Hide other contents
+	// 		clearActiveDivs();
+
+	// 		// Show current content
+	// 		activateDiv('#experienceContent');
+	// 	}
+	// });
+
+	// Handle 'Projects' content
+	// $('#projects').click(function(e) {
+
+	// 	// If the div has already the class active, no need to reload the divs...
+	// 	if(!$(e.target).hasClass('active')) {
+	// 		// Update navbar
+	// 		clearActiveLinks();
+	// 		activateLink(e);
+
+	// 		// Hide other contents
+	// 		clearActiveDivs();
+
+	// 		// Show current content
+	// 		activateDiv('#projectsContent');
+	// 	}
+	// });
+
+	/*
+	// Handle 'Template' content
+	$('#name').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#nameContent');
+		}
+	});
+	*/
+
+	// Whenever you clic on a blog post, you should be redirected to that post' html
+	$('.clickable').click(function(e) {
+		window.open($(e.currentTarget)[0].childNodes[1].innerText, '_blank').focus();
+	});
+
+	// Whenever you clic on a clases particulares heading, you should toggle
+	$('.particular-clickable').click(function(e) {
+		var e = $(e.currentTarget)[0].nextElementSibling;
+		if (e.style.display === "none") {
+			e.style.display = "block";
+		}
+		else {
+			e.style.display = "none";
+		}
+	});
+
+	// Copy the citation to the clipboard
+	// THIS SHOULD BE THE SAME FOR ALL THE PAPERS
+	$(document).on("click", "#citation", function(){
+		var text = $(this).parent().parent().next()[0].innerHTML;
+
+		navigator.clipboard.writeText(text);
+
+		toastr.success('Citation copied');
+	});
+
+	// Controls the URL; if it has '#blog'
+	// then trigger the 'Blog' clic
+	// if (((window.location).href).substring(((window.location).href).lastIndexOf('#') + 1) == 'tutorials') {
+	// 	$('#tutorials').click();
+	// 	$('#tutorialsContent').focus();
+	// }
+
+	if (((window.location).href).substring(((window.location).href).lastIndexOf('#') + 1) == 'clases-particulares') {
+		$('#particular').click();
+		$('#particularContent').focus();
+	}
+
+	// Controls the options menu
+	$('#options-toggler').click(function(e) {
+		if(!$(e.currentTarget).hasClass('active')) {
+			$(e.currentTarget).addClass('active');
+			$('#theme').show("fast");
+			$('#lan').show("fast");
+		}
+		else {
+			$(e.currentTarget).removeClass('active');
+			$('#theme').hide("fast");
+			$('#lan').hide("fast");
+		}
+	})
+
+	// Animates the theme button + functionality
+	$('#theme').click(function(e) {
+		if(localStorage.theme != "dark"){
+
+			$('#theme').empty().append("<i class='fa-duotone fa-lightbulb-slash'></i>");
+
+			localStorage.theme = "dark"
+			
+			$("link[href='assets/css/light.css']").remove();
+			$('<link>').appendTo('head').attr({
+				type: 'text/css', 
+				rel: 'stylesheet',
+				href: 'assets/css/dark.css'
+			});
+		}
+		else {
+
+			$('#theme').empty().append("<i class='fa-duotone fa-lightbulb'></i>");
+
+			localStorage.theme = "light"
+			
+			$("link[href='assets/css/dark.css']").remove();
+			$('<link>').appendTo('head').attr({
+				type: 'text/css', 
+				rel: 'stylesheet',
+				href: 'assets/css/light.css'
+			});
+		}
+	})
+
+	// Animates the lan button + functionality
+	$('#lan').click(function(e) {
+		if(!$(e.currentTarget).hasClass('es')){
+			$(e.currentTarget).addClass('es');
+
+			$('#lan img').attr("src","/assets/img/es_flag.webp");
+
+			localStorage.lan = "es"
+		}
+		else {
+			$(e.currentTarget).removeClass('es');
+
+			$('#lan img').attr("src","/assets/img/en_flag.webp");
+
+			localStorage.lan = "en"
+		}
+
+		updateLanguage();
+	})
+
+});
+
+function updateLanguage() {
+	let lang = localStorage.lan;
+	$(".language *").each(function(){
+		$(this).html( $(this).data(lang) );
+	});
 }
-showMenu('nav-toggle','nav-menu')
 
-/*===== REMOVE MENU MOBILE =====*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
-const sections = document.querySelectorAll('section[id]')
-
-window.addEventListener('scroll', scrollActive)
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
+function clearActiveLinks() {
+	$('#navbarList .nav-item .nav-link').each(function() {
+		$(this).removeClass('active');
+	});
 }
 
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-})
-
-/*SCROLL HOME*/
-sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'right', delay: 400})
-
-/*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 200})
-
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
-
-/*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 200})
-
-/*SCROLL CONTACT*/
-sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {interval: 200})
-sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
-
-
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+function clearActiveDivs() {
+	$('.container .content .active').each(function() {
+		$(this).removeClass('active');
+		$(this).hide();
+	});
 }
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+function activateLink(e) {
+	$(e.target).addClass('active');
+	
+	// Hide left panel
+	if(e.target.id == "particular")
+		$('#leftPanel').hide();
+	else
+		$('#leftPanel').show();
+}
 
+function activateDiv(divId) {
+	$(divId).addClass('active');
+	$(divId).show();
+
+	// Scrolls to the content
+	scrollToContent(divId);
+}
+
+function scrollToContent(divId) {
+	if ($(window).width() < 751) {
+		$('html, body').animate({
+			scrollTop: $(divId).offset().top
+		}, 1);
+	}
+}
+
+function resetViews() {
+	$.getJSON("https://api.countapi.xyz/set/nicolasmeseguer.github.io/634c2142-b35d-430e-b51c-dad16880dd3a?value=0", function(response) {
+		$("#contadorVisitas").text("0");
+	});
+}
